5e09bcb6daa4eb22afd263cf26dae34b135b3611
Michelle <michelle.cevallos93@hotmail.com>
2023-07-22 16:04:52 -0500

    añadi imagenes

---
+++ clase22_julio.R
@@ -0,0 +1,21 @@
+#Curso Rmarkdown
+
+En este repositorio se almacena el codigo y los archivos del curso rmarkdown New Dimension 
+Aqui va mi siente parrafo 
+
+
+Este es el tercer parrafo.
+Este es un cuarto parrafo sin espacio
+este es un quinto parrafo sin espacio
+
+Los profesores del New Dimension son:
+  
+Diana
+Alex
+Angel
+Zulemna
+Alonso
+
+
+
+
